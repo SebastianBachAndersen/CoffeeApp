b3b05cdb4f4932858f94fdc73da87ae31a5c2198
ulrik Magnusson <ulri1122@gmail.com>
2021-05-12 13:58:53 +0200

    seach bar + search :DDD

--- lib/Pages/search_view.dart
+++ lib/Pages/search_view.dart
@@ -16,6 +16,79 @@ class _State extends State<SearchView> {
           backgroundColor: Color(0xff49281A),
           title: const Text('Search'),
         ),
+        body: Center(child: ListSearch()),
+      ),
+    );
+  }
+}
+
+class ListSearch extends StatefulWidget {
+  ListSearchState createState() => ListSearchState();
+}
+
+class ListSearchState extends State<ListSearch> {
+  TextEditingController _textController = TextEditingController();
+
+  static List<String> mainDataList = [
+    "Apple",
+    "Apricot",
+    "Banana",
+    "Blackberry",
+    "Coconut",
+    "Date",
+    "Fig",
+    "Gooseberry",
+    "Grapes",
+    "Lemon",
+    "Litchi",
+    "Mango",
+    "Orange",
+    "Papaya",
+    "Peach",
+    "Pineapple",
+    "Pomegranate",
+    "Starfruit"
+  ];
+
+  // Copy Main List into New List.
+  List<String> newDataList = List.from(mainDataList);
+
+  onItemChanged(String value) {
+    if (value == "") {}
+    setState(() {
+      newDataList = mainDataList
+          .where((string) => string.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                hintText: 'Search Here...',
+              ),
+              onChanged: onItemChanged,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.all(12.0),
+              children: newDataList.map((data) {
+                return ListTile(
+                  title: Text(data),
+                  onTap: () => print(data),
+                );
+              }).toList(),
+            ),
+          )
+        ],
       ),
     );
   }
